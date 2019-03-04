bb966fe567fb193fba965de8b80dfa8369665426
Matt-1123 <matthew.russo91@gmail.com>
2019-03-04 16:22:56 -0500

    add Counters component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,29 +2,30 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value;
   };
 
-  handleIncrement = () => {
-    console.log(this.state.count);
-  }
+  handleIncrement = product => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
-
-    return (
-      <React.fragment>
+    return(
+      <React.fragment >
+        <span className = { this.getBadgeClasses() } > { this.formatCount() }</span >
         <button
-          onClick={this.handleIncrement}
-          className="btn btn-secondary btn-sm">
+          onClick = {() => this.handleIncrement(product)}
+          className = "btn btn-secondary btn-sm" >
           Increment
         </button>
-      </React.fragment>
-
+      </React.fragment >
     );
+
+    getBadgeClasses() {
+      let classes = "badge m-2 badge-";
+      classes += this.state.count === 0 ? "warning" : "primary";
+      return classes;
+    };
   }
 
-}
-
-
-
-export default Counter;
+export default Counter; 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/Counter';
+import Counters from './components/Counters';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counters />, document.getElementById('root'));
 
 serviceWorker.unregister();
